89499586aef3701f927e3add9c534588bcbc2a38
Chris Mercer <chriswm@me.com>
2020-05-25 12:16:19 +0100

    Initial Project Creation

---
+++ TripleLocked/TripleLocked/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TripleLocked
+//
+//  Created by Chris Mercer on 25/05/2020.
+//  Copyright © 2020 Chris Mercer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
